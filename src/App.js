698cbb12e9774fc7f3a1c6978569f8062bbb8306
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-07-24 04:05:27 -0700

    use links of different category

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import RootesLayout from './Component/Rootes';
-import Home from "./Component/Pages/Home"
+import Home from "./Component/Pages/Home";
+import Business from './Component/Pages/Business';
+import Technology from './Component/Pages/Technology';
+import Entertainment from './Component/Pages/Entertainment';
+import Health from './Component/Pages/Health';
+import Science from './Component/Pages/Science';
+import Sports from './Component/Pages/Sports';
 function App() {
- 
-  const Router=createBrowserRouter([
-    {path:"/",element:<RootesLayout/>,children:[
-      {index:true,element:<Home/>}
-    ]}
+
+  const Router = createBrowserRouter([
+    {
+      path: "/", element: <RootesLayout />, children: [
+        { index: true, element: <Home /> },
+        { path: "sports", element: <Sports /> },
+        { path: "bussines", element: <Business /> },
+        { path: "Technology", element: <Technology /> },
+        { path: "entertainment", element: <Entertainment /> },
+        { path: "health", element: <Health /> },
+        { path: "science", element: <Science /> }
+      ]
+    }
   ])
 
   return (
     <>
-    <RouterProvider router={Router}/>
+      <RouterProvider router={Router} />
     </>
   );
 }
