034f95a99e77dafd85c007f620848a75fdd7cef1
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-07-25 09:50:45 -0700

    update all new component as same Home component

--- src/Component/Pages/Science.js
+++ src/Component/Pages/Science.js
@@ -1,39 +1,91 @@
-
+import React, { useEffect, useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Card from "../UI/Card";
-import { useEffect, useState } from "react";
+import LoaderBar from "../Validation/LoaderBar";
+import Error from "../Validation/Error";
+
 const Science = () => {
-    const [Data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [pageSize, setPageSize] = useState(5)
+    const [isDesibled, setDesibled] = useState(false)
+    const nextPageHandler = () => {
+        setPageSize(pageSize + 5)
+
+    }
+    const previousPageHadler = () => {
+        if (pageSize === 5) {
+            setDesibled(true)
+        }
+        setPageSize(pageSize - 5)
+    }
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(" https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=32ad1ec8ba01499293a3492f3141822e")
-            const data = await response.json();
-            setData(data.articles)
-        }
+            try {
+                setProgress(20);
+                const response = await fetch(
+                    `https://newsapi.org/v2/top-headlines?country=in&pageSize=${pageSize}&category=science&apiKey=32ad1ec8ba01499293a3492f3141822e`
+                );
+                setProgress(40);
+                if (!response.ok) {
+                    throw new Error("Could not fetch data " + response.status);
+                }
+                setProgress(70);
+                const data = await response.json();
+                setData(data.articles);
+                setLoading(true);
+                setProgress(100);
+            } catch (error) {
+                setLoading(false);
+                setError("could not fetch Data ! some error occured");
+                setProgress(100)
+            }
+        };
+
         fetchData();
-    }, [])
+    }, [pageSize]);
 
     return (
-
-        <Card>
-            {Data.map((news) => {
-                return (
-                    <div key={news.url}>
-                        <div className="card" style={{ width: "18rem", margin: "2rem" }} >
-                            {news.img}
-                            <div className="card-body">
-                                <img src={news.urlToImage} className="card-img-top" alt="..." />
-                                <h5 className="card-title">{news.title}</h5>
-                                <p className="card-text">{news.description}</p>
-                                <p className="card-date">{news.date}</p>
-                                <Link to={news.url} className="btn btn-primary">Read More</Link>
+        <Fragment>
+            {!loading ? (
+                <LoaderBar progress={progress} />
+            ) : (
+                <div>
+                    <Card>
+                        {data.map((news) => (
+                            <div
+                                key={news.url}
+                                className="card"
+                                style={{ width: "18rem", margin: "2rem" }}
+                            >
+                                <img src={news.urlToImage} className="card-img-top" alt="error" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{news.title}</h5>
+                                    <p className="card-text">{news.description}</p>
+                                    <p className="card-date">{news.publishedAt}</p>
+                                    <Link to={news.url} className="btn btn-primary">
+                                        Read More
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </Card>
+                        ))}
 
-    )
-}
+                    </Card>
+                    <div className="button">
+                        <button onClick={previousPageHadler} disabled={isDesibled}>Previous</button>
+                        <button onClick={nextPageHandler}>Next</button>
+                    </div>
+                </div>
+            )
+
+            }
+            <br />
+
+            {!loading && error && <Error message={error} />}
+        </Fragment>
+    );
+};
+
 export default Science;
