2dee47a7459a84b2e0069a10cbc93e31ef9f0e12
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-03 04:18:14 -0700

    create custom Hook file

---
+++ src/Component/Pages/CustomHook.js
@@ -0,0 +1,96 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Card from "../UI/Card";
+import LoaderBar from "../Validation/LoaderBar";
+import Error from "../Validation/Error";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { NextPageHandler } from "../../Component/NextPageHandler";
+const CustomHook = ({ category, pageSize }) => {
+    const [data, setData] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const nextPageHandler = async () => {
+        <NextPageHandler data="data" category={category} pageSize={pageSize} setError={setError} setData={setData} />
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setProgress(20);
+                const response = await fetch(
+                    `https://newsapi.org/v2/top-headlines?country=in&pageSize=${pageSize}&category=${category}&apiKey=32ad1ec8ba01499293a3492f3141822e`
+                );
+                setProgress(40);
+                if (!response.ok) {
+                    throw new Error("Could not fetch data " + response.status);
+                }
+                setProgress(70);
+                const data = await response.json();
+                setData(data.articles);
+                setLoading(true);
+                setProgress(100);
+            } catch (error) {
+                setLoading(false);
+                setError("could not fetch Data ! some error occured");
+                setProgress(100)
+            }
+        };
+
+        fetchData();
+    }, [pageSize, category]);
+    return (
+        <div>
+            <Fragment>
+                <InfiniteScroll
+                    dataLength={data.length}
+                    next={nextPageHandler}
+                    hasMore={true}
+                    // loader={<Loader />}
+                    scrollableTarget="scrollableDiv"
+                >
+                    {!loading ? (
+                        <div>
+                            <LoaderBar progress={progress} />
+
+                        </div>
+
+                    ) : (
+                        <div>
+                            <Card>
+                                {data.map((news) => (
+                                    <div
+                                        key={news.url}
+                                        className="card"
+                                        style={{ width: "18rem", margin: "2rem" }}
+                                    >
+                                        <img src={news.urlToImage} className="card-img-top" alt="error" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{news.title}</h5>
+                                            <p className="card-text">{news.description}</p>
+                                            <p className="card-date">{news.publishedAt}</p>
+                                            <Link to={news.url} className="btn btn-primary">
+                                                Read More
+                                            </Link>
+                                        </div>
+                                    </div>
+                                ))}
+
+                            </Card>
+
+                        </div>
+                    )
+
+                    }
+                    <br />
+                    {!loading && error && <Error message={error} />}
+                </InfiniteScroll>
+            </Fragment>
+        </div>
+    )
+};
+
+export default CustomHook;
+
+
+
