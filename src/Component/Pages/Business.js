d0cba92787554531947795f2005dba6fa2a70bd3
Muhsan <muhsansattar97@gmail.com>
2025-01-08 18:04:08 +0500

    fix comflict

--- src/Component/Pages/Business.js
+++ src/Component/Pages/Business.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React, { Fragment } from "react";
-
-const Business = () => {
-
-
-    return (
-        <Fragment>
-            {/* <CustomHook category="business" pageSize="6" /> */}
-
-        </Fragment>
-    );
-};
-
-export default Business;
-=======
 import React, { Fragment } from "react";
 import CustomHook from "./CustomHook";
 const Business = () => {
@@ -28,4 +12,3 @@ const Business = () => {
 };
 
 export default Business;
->>>>>>> 73d6e7f40c0efff7f8766d14a2f85aea331e2bcf
